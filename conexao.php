f8584fefd7911790d00c211eb438be6945092f16
Igor Felipe <igorhdg@gmail.com>
2022-08-02 23:31:23 -0300

    host notebook form bd

--- conexao.php
+++ conexao.php
@@ -1,14 +1,14 @@
 <?php
-define('host', 'localhost');
-define('usuario', 'root');
+define('host', '192.168.0.109');
+define('usuario', 'tccnote');
 define('senha', '12345');
 define('db','tccdrip');
 
 
 $conexao = mysqli_connect(host, usuario, senha, db) or die ("Não foi possível conectar");
 
-$connection = 'mysql:dbname=tccdrip;host=localhost';
-$user = 'root';
+$connection = 'mysql:dbname=tccdrip;host=192.168.0.109';
+$user = 'tccnote';
 $password = '12345';
 
 $pdo = new PDO($connection, $user, $password);
