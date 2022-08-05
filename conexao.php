28c330fb1ffdc168ede73b3c490ada1a2fb26342
Igor Felipe <igorhdg@gmail.com>
2022-08-04 23:53:59 -0300

    ip antigo volto

--- conexao.php
+++ conexao.php
@@ -1,5 +1,5 @@
 <?php
-define('host', '192.168.1.4');
+define('host', '192.168.0.109');
 define('usuario', 'tccnote');
 define('senha', '12345');
 define('db','tccdrip');
@@ -7,7 +7,7 @@ define('db','tccdrip');
 
 $conexao = mysqli_connect(host, usuario, senha, db) or die ("Não foi possível conectar");
 
-$connection = 'mysql:dbname=tccdrip;host=192.168.1.4';
+$connection = 'mysql:dbname=tccdrip;host=192.168.0.109';
 $user = 'tccnote';
 $password = '12345';
 
