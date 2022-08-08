4e33c70142d3e86b0b05ec91788da1c518a72568
Igor Felipe <igorhdg@gmail.com>
2022-08-07 23:19:44 -0300

    removendo comentários

--- estados.js
+++ estados.js
@@ -13,15 +13,11 @@ const pegarestados = async() => {
 
 const inserirestados = (data) =>{
 
-    //estado_cadastro.innerHTML = " "
-    //estado_cadastro.innerHTML = "<option>Selecione</option>"
-
     data.forEach(row => {
         var teste = document.createElement('option');
         teste.value = `${row["cod_estado"]}`;
         teste.innerHTML = `${row["est_nome"]}`;
         estado_cadastro.appendChild(teste);
-     //estado_cadastro.innerHTML += `<option value="${row["cod_estado"]}">${row["est_nome"]}</option>`
     })
 
     estado_cadastro.addEventListener("change", e => pegarcidades(estado_cadastro.value))
@@ -44,7 +40,6 @@ const inserircidades = (data) =>{
         teste2.value = `${row["cid_nome"]}`;
         teste2.innerHTML = `${row["cid_nome"]}`;
         cidade_cadastro.appendChild(teste2);
-    //cidade_cadastro.innerHTML += `<option value="${row["cid_nome"]}">${row["cid_nome"]}</option>`
     })
 
 }
