9db0519f9e7065db99dd8f387197b7891b08340d
Igor Felipe <igorhdg@gmail.com>
2022-08-08 22:45:40 -0300

    tentando arrumar substring

--- includes/login.php
+++ includes/login.php
@@ -12,10 +12,10 @@ $email = mysqli_real_escape_string($conexao, $_POST['email_login']);
 $senha = mysqli_real_escape_string($conexao, $_POST['senha_login']);
 
 $query = "select cod_cliente, cli_email from tb_cliente where cli_email = '{$email}' and cli_senha = '{$senha}'";
-$query2 = mysqli_query($conexao, "select cli_nome from tb_cliente where cli_email = '{$email}'");
-while ($row = mysqli_fetch_assoc($query2)){
-    $usuario_cli = $row['cli_nome'];
-}
+$query2 = mysqli_query($conexao, "select substring(cli_nome, 2, 5) from tb_cliente where cli_email = '{$email}'");
+//while ($row = mysqli_fetch_assoc($query2)){
+    //$usuario_cli = $row['cli_nome'];
+//}
 $resultado = mysqli_query($conexao, $query);
 
 $row = mysqli_num_rows($resultado);
@@ -23,11 +23,11 @@ $row = mysqli_num_rows($resultado);
 if($row == 1){
     $_SESSION['login_completo'] = true;
     $_SESSION['usuario_cli'] = $usuario_cli;
-    header('location: \tcc/index.php');
+    //header('location: \tcc/index.php');
     exit();
 } else{
     $_SESSION['login_incompleto'] = true;
-    header('location: \tcc/index.php');
+    //header('location: \tcc/index.php');
     exit();
 }
 ?> 
