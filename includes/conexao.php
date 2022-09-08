8ea62b1912e04e121719fc203d1c2f024fe394f0
Igor Felipe <igorhdg@gmail.com>
2022-09-08 10:26:23 -0300

    nada

--- includes/conexao.php
+++ includes/conexao.php
@@ -1,5 +1,5 @@
 <?php
-define('host', '192.168.0.104');
+define('host', 'localhost');
 define('usuario', 'tccnote');
 define('senha', '12345');
 define('db','tccdrip');
@@ -7,7 +7,7 @@ define('db','tccdrip');
 
 $conexao = mysqli_connect(host, usuario, senha, db) or die ("Não foi possível conectar");
 
-$connection = 'mysql:dbname=tccdrip;host=192.168.0.104';
+$connection = 'mysql:dbname=tccdrip;host=localhost';
 $user = 'tccnote';
 $password = '12345';
 
