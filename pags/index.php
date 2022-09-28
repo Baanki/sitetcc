64f612755cde848ffa1dbea86030ac81716feb8d
Igor Felipe <igorhdg@gmail.com>
2022-09-27 23:37:42 -0300

    Continuando produtos

--- pags/index.php
+++ pags/index.php
@@ -7,6 +7,25 @@ include_once('../includesfront/header.php');
     <link rel="stylesheet" type="text/css" href="../styles/index.css">
     <script src="../scripts/carousel.js" defer></script>
 </head>
+<?php
+
+
+
+
+
+    $query = 'select prod_nome from tb_produto where cod_produto=1';
+    $aaa = mysqli_query($conexao, $query);
+    $query2 = 'select prod_preco from tb_produto where cod_produto=1';
+    $aaa2 = mysqli_query($conexao, $query2);
+    $query3 = 'select prod_imagem from tb_produto where cod_produto=1';
+    $aaa3 = mysqli_query($conexao, $query3);
+    while ($row = mysqli_fetch_assoc($aaa) and $row2 = mysqli_fetch_assoc($aaa2) and $row3 = mysqli_fetch_assoc($aaa3)){
+        $nome = $row['prod_nome'];
+        $preco = $row2['prod_preco'];
+        $img = $row3['prod_imagem'];
+    }
+    
+?>
 <body>
     <div class="carousel_marca">
         <div class="carousel">
@@ -23,7 +42,16 @@ include_once('../includesfront/header.php');
             <div class="produto_itens">
                 <picture>
                     <a href="#">
-                        <img src="../img/teste_produto.jpg" class="produto_imagem">
+                        <img src="http://localhost/tcc/img/teste_produto.jpg" class="produto_imagem">
+                    </a>
+                </picture>
+                <p class="produto_nome"><?php echo($nome)?></p>
+                <p class="produto_preco">R$ <?php echo($preco)?></p>
+            </div>
+            <div class="produto_itens">
+                <picture>
+                    <a href="#">
+                        <img src="<?php echo($img) ?>" class="produto_imagem">
                     </a>
                 </picture>
                 <p class="produto_nome">Moletom Pica</p>
@@ -32,7 +60,7 @@ include_once('../includesfront/header.php');
             <div class="produto_itens">
                 <picture>
                     <a href="#">
-                        <img src="../img/teste_produto.jpg" class="produto_imagem">
+                        <img src="../img/teste_produto3.jpg" class="produto_imagem">
                     </a>
                 </picture>
                 <p class="produto_nome">Moletom Pica</p>
@@ -41,16 +69,7 @@ include_once('../includesfront/header.php');
             <div class="produto_itens">
                 <picture>
                     <a href="#">
-                        <img src="../img/teste_produto.jpg" class="produto_imagem">
-                    </a>
-                </picture>
-                <p class="produto_nome">Moletom Pica</p>
-                <p class="produto_preco">R$500,00</p>
-            </div>
-            <div class="produto_itens">
-                <picture>
-                    <a href="#">
-                        <img src="../img/teste_produto.jpg" class="produto_imagem">
+                        <img src="../img/teste_produto3.jpg" class="produto_imagem">
                     </a>
                 </picture>
                 <p class="produto_nome">Moletom Pica</p>
@@ -59,6 +78,8 @@ include_once('../includesfront/header.php');
         </div>    
     </section>
 </body>
+
+
 <script>
 
 </script>
